608fe9c5acb8d67d877c7a7a3944929a4cf5f9e4
smit patel <bhalodiyasmit2409@gmail.com>
2026-01-01 19:36:08 +0530

    feat: enhance SEO and add sitemap integration; include new favicon and manifest files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,30 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://shadcnstudio.com/",
+  integrations: [
+    react(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    })
+  ],
   output: "static",
-
+  compressHTML: true,
+  build: {
+    inlineStylesheets: "auto",
+  },
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      cssMinify: true,
+      minify: "esbuild",
+    },
   },
 });
